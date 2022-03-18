280aefa24b803ffc5d52130e309a671666f3a44a
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-03-18 17:56:45 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -9,9 +9,81 @@ l2011 <- brick("p224r63_2011.grd")
 
 l2011 # plot
 
-plot(l2011) # https://www.r-graph-gallery.com/42-colors-names.html
+plot(l2011)
 
+# https://www.r-graph-gallery.com/42-colors-names.html
 cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
 
-plot(l2011, col=cl) # dev.off()  
+plot(l2011, col=cl) 
+
+# dev.off()  
+
+# Landsat ETM+
+# b1 = blu
+# b2 = verde
+# b3 = rosso
+# b4 = infrarosso vicino NIR
+
+# plot della banda del blu - B1_sre 
+plot(l2011$B1_sre) # plotto il nome dell'oggetto
+# or
+plot(l2011[[1]]) #plotto l'elemento numero 1
+
+plot(l2011$B1_sre)
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(l2011$B1_sre, col=cl)
+
+# plot b1 from dark blue to blue to light blue
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(l2011$B1_sre, col=clb)
+
+# let's export the image and let it appear in the lab folder: kind of magic
+pdf("banda1.pdf") # dice solo qual è il nome del pdf da creare
+plot(l2011$B1_sre, col=clb)
+dev.off()
+
+# rifaccio con png
+png("banda1.png") 
+plot(l2011$B1_sre, col=clb)
+dev.off()
+
+# plot b1 from dark green to green to light green
+clg <- colorRampPalette(c("dark green", "green", "light green")) (100)
+plot(l2011$B2_sre, col=clg)
+
+# multiframe
+par(mfrow=c(1,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+dev.off() #mettilo solo se devi chiuderle se no continua a fare finestre a 2 img
+
+# export multiframe plot
+pdf("multiframe.pdf")
+par(mfrow=c(1,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+dev.off()
+
+# exercise: revert the multiframe
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+dev.off()
+
+# let's plot the first four bands
+par(mfrow=c(2,2))
+# blue
+plot(l2011$B1_sre, col=clb)
+# green
+plot(l2011$B2_sre, col=clg)
+# red
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(l2011$B3_sre, col=clr)
+# NIR
+clnir <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(l2011$B4_sre, col=clnir)
+
+
+
+
 
