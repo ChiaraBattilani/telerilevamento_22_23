a8ae0053fb300be7b14038c4879df1c1bb36c203
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-03-18 16:29:10 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,17 @@
 # Questo è il primo script che useremo a lezione
 
 library(raster)
+
+#Settaggio cartella di lavoro 
+setwd("C:/lab") # Windows
+
+l2011 <- brick("p224r63_2011.grd")
+
+l2011 # plot
+
+plot(l2011) # https://www.r-graph-gallery.com/42-colors-names.html
+
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+
+plot(l2011, col=cl) # dev.off()  
+
