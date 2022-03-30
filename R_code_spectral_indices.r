5885fd0c4a7e85658a4eff7273d4bededb19b002
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-03-30 10:52:19 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -3,4 +3,25 @@ library(raster)
 # Settaggio WD in Windows
 setwd("C:/lab")
 
-# import the first file -> defor1.jpg -> give it the name l1992
+# Exercise: import the first file -> defor1_.jpg -> give it the name l1992
+l1992 <- brick("defor1_.jpg")
+
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+
+# layer 1 = NIR
+# layer 2 = red
+# layer 3 = green
+
+# Exercise: import the second file -> defor2_.jpg -> give it the name l2006
+l2006 <- brick("defor2_.jpg")
+
+plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
+
+# Exercise: plot in multiframe the two images with one on top of the other
+
+
+
+# DVI Difference Vegetation Index
+
+
+
