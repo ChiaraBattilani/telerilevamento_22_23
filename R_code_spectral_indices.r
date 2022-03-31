d45216571054a1719cd7f303d4f3edcdd417f5c6
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-03-31 17:45:33 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -1,4 +1,7 @@
 library(raster)
+library(RStoolbox)
+# install.packages("rasterdiv")
+library(rasterdiv)
 
 # Settaggio WD in Windows
 setwd("C:/lab")
@@ -24,9 +27,9 @@ plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
 # DVI Difference Vegetation Index 1992
 # Potevo usare anche assegnazione <-
-dvi1992 = l1992[[1]] - l1992[[2]]
+dvi1992 = l1992[[1]] - l1992[[2]] # uso elemento
 # or:
-# dvi1992 = l1992$defor1_.1- l1992$defor1_.2
+# dvi1992 = l1992$defor1_.1- l1992$defor1_.2 #uso il nome
 dvi1992
 
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
@@ -44,4 +47,54 @@ dvi_dif = dvi1992 - dvi2006
 cld <- colorRampPalette(c('blue','white','red'))(100)
 dev.off()
 plot(dvi_dif, col=cld)
+
+# Day 2
+
+# Range DVI (8 bit): -255 a 255
+# Range NDVI (8 bit): -1 a 1
+
+# Range DVI (16 bit): -65535 a 65535
+# Range NDVI (16 bit): -1 a 1
+
+# Quindi l'NDVI può essere usato anche con immagini con risoluzione radiometrica differente
+
+# NDVI 1992
+dvi1992 = l1992[[1]] - l1992[[2]]
+ndvi1992 = dvi1992 / (l1992[[1]] + l1992[[2]])
+# or
+# ndvi1992 = (l1992[[1]] - l1992[[2]]) / (l1992[[1]] + l1992[[2]])
+
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+plot(ndvi1992, col=cl)
+
+# Multiframe with plotRGB on top of the NDVI image
+par(mfrow=c(2,1))
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+plot(ndvi1992, col=cl)
+
+# NDVI 2006
+dvi2006 = l2006[[1]] - l2006[[2]]
+ndvi2006 = dvi2006 / (l2006[[1]] + l2006[[2]])
+
+# Multiframe with NDVI1992 image on top of the NDVI2006
+par(mfrow=c(2,1))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
+
+# Automatic spectral indices by the spectralIndices
+si1992 <- spectralIndices(l1992, green=3, red=2, nir=1)
+plot(si1992, col=cl)
+
+si2006 <- spectralIndices(l2006, green=3, red=2, nir=1)
+plot(si2006, col=cl)
+
+### rasterdiv
+plot(copNDVI)
+
+
+
+
+
+
+
 
