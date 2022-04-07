29fec024ee9f528d4370fc596218789a0e63c14a
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-04-07 17:46:17 +0200

    Update R_code_time_series_greenland.r

--- R_code_time_series_greenland.r
+++ R_code_time_series_greenland.r
@@ -36,6 +36,47 @@ plot(tgr[[1]], col=cl) # plot del 1° elemento
 
 plotRGB(tgr, r=1, g=2, b=3, stretch="lin") # sovrapposizione temporale
 
+######################################################
+### Example 2: NO2 decrease during the lockdown period
+######################################################
+
+setwd("C:/lab/EN") # Windows
+
+# Importo il primo layer
+en01 <- raster("EN_0001.png")
+
+cl <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(en01, col=cl)
+
+en13 <- raster("EN_0013.png")
+plot(en13, col=cl)
+
+# Let's import the whole set (altogether!)
+
+# Exercise: import the whole as in the Greenland example by the following steps:
+# list.files, lapply, stack
+
+rlist <- list.files(pattern="EN") # creo la lista
+rimp <- lapply(rlist, raster) # applico alla lista la funzione raster
+en <- stack(rimp) # crea un unico file con i 13 layer
+
+plot(en, col=cl) # plotto tutto assieme
+
+# Exercise: plot EN01 besides EN13
+par(mfrow=c(1,2)) # modo 1 con multiframe
+plot(en[[1]], col=cl)
+plot(en[[13]], col=cl)
+
+# or:
+en113 <- stack(en[[1]], en[[13]]) # modo 2 con stack
+plot(en113, col=cl)
+
+# let's make the difference:
+difen <- en[[1]] - en[[13]]
+cldif <- colorRampPalette(c('blue','white','red'))(100)
+plot(difen, col=cldif)
+
+
 
 
 
