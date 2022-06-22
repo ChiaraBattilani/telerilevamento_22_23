366bc1e527681fdf29d14a71b8371ce8a1752071
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-06-22 16:59:01 +0200

    Update R_code_colorist.r

--- R_code_colorist.r
+++ R_code_colorist.r
@@ -31,22 +31,22 @@ map_single(met1_distill, pal) # parti più colorate hanno una maggior specificit
 # riguardano quelle con minor specificità, ovvero in questo caso significa che le specie sono presenti
 # nelle zone grigie per la maggior parte dei mesi.
 
+# legenda
+legend_timecycle(pal, origin_label = "1 jan") 
 
+# mappare il comportamento individuale di un individuo nel tempo, non è più un tempo circolare, è lineare
 
+# fisher ----
+data("fisher_ud")
+m2 <- metrics_pull(fisher_ud)
+pal2 <- palette_timeline(fisher_ud)
+head(pal2)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# map multiple
+map_multiples(m2, pal2, ncol = 3, lambda_i = -12) # lambda lavora sull'opacità
+m2_distill <- metrics_distill(fisher_ud)
+map_single(m2_distill, pal2, lambda_i = -10)
+legend_timeline(pal2)
 
 
 
