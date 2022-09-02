f861f619441d69b902f367bc501b6ca093159013
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-09-02 16:08:44 +0200

    Update 02_R_code_spectral_indices.r

--- 02_R_code_spectral_indices.r
+++ 02_R_code_spectral_indices.r
@@ -16,24 +16,24 @@ plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 # layer 2 = red
 # layer 3 = green
 
-# Esercizio: importa il primo file defor2_.jpg e dagli il nome l2006
+# Esercizio: importa il secondo file defor2_.jpg e dagli il nome l2006
 l2006 <- brick("defor2_.jpg")
 
 plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
-# Esercizio: plot in un multiframe le due immagini una sopra all'altra
+# Esercizio: plotta in un multiframe le due immagini una sopra all'altra
 par(mfrow=c(2,1))
 plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
-# DVI Difference Vegetation Index 1992
+# DVI (Difference Vegetation Index) 1992: la differenza tra le riflettanze nel NIR e nel rosso
 # Potevo usare anche assegnazione <-
 dvi1992 = l1992[[1]] - l1992[[2]] # uso elemento
-# or:
-# dvi1992 = l1992$defor1_.1- l1992$defor1_.2 #uso il nome
-dvi1992
+# o:
+# dvi1992 = l1992$defor1_.1- l1992$defor1_.2 # uso il nome
+dvi1992 # visualizzo i parametri
 
-cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # creo una scala di colori differente
 plot(dvi1992, col=cl)
 
 # DVI Difference Vegetation Index 2006
@@ -43,13 +43,11 @@ dvi2006
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
 plot(dvi2006, col=cl)
 
-# DVI difference in time
+# differenza DVI nel tempo
 dvi_dif = dvi1992 - dvi2006
 cld <- colorRampPalette(c('blue','white','red'))(100)
 dev.off()
 plot(dvi_dif, col=cld)
-
-# Day 2
 
 # Range DVI (8 bit): -255 a 255
 # Range NDVI (8 bit): -1 a 1
@@ -57,18 +55,18 @@ plot(dvi_dif, col=cld)
 # Range DVI (16 bit): -65535 a 65535
 # Range NDVI (16 bit): -1 a 1
 
-# Quindi l'NDVI può essere usato anche con immagini con risoluzione radiometrica differente
+# Quindi l'NDVI può essere usato anche con immagini con risoluzione radiometrica differente, è meglio
 
 # NDVI 1992
 dvi1992 = l1992[[1]] - l1992[[2]]
 ndvi1992 = dvi1992 / (l1992[[1]] + l1992[[2]])
-# or
+# o
 # ndvi1992 = (l1992[[1]] - l1992[[2]]) / (l1992[[1]] + l1992[[2]])
 
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
 plot(ndvi1992, col=cl)
 
-# Multiframe with plotRGB on top of the NDVI image
+# Multiframe con plotRGB sopra e NDVI sotto
 par(mfrow=c(2,1))
 plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 plot(ndvi1992, col=cl)
@@ -77,25 +75,18 @@ plot(ndvi1992, col=cl)
 dvi2006 = l2006[[1]] - l2006[[2]]
 ndvi2006 = dvi2006 / (l2006[[1]] + l2006[[2]])
 
-# Multiframe with NDVI1992 image on top of the NDVI2006
+# Multiframe con NDVI1992 sopra e NDVI2006 sotto
 par(mfrow=c(2,1))
 plot(ndvi1992, col=cl)
 plot(ndvi2006, col=cl)
 
-# Automatic spectral indices by the spectralIndices
+# Calcolo automaticamente gli indici spettrali dell'immagine del 1992 e del 2006 tramite la funzione "spectralIndices"
 si1992 <- spectralIndices(l1992, green=3, red=2, nir=1)
 plot(si1992, col=cl)
 
 si2006 <- spectralIndices(l2006, green=3, red=2, nir=1)
 plot(si2006, col=cl)
 
-### rasterdiv
+# Carico il pacchetto "rasterdiv" e faccio il plot
 plot(copNDVI)
-
-
-
-
-
-
-
 
