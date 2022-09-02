3649cde2c07cd5c5ad9360b456602381d0b28d4d
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-09-02 16:41:55 +0200

    Update 03_R_code_time_series_greenland.r

--- 03_R_code_time_series_greenland.r
+++ 03_R_code_time_series_greenland.r
@@ -1,19 +1,20 @@
 # Time series analysis of Greenland LST data
 
+# Carico e installo i pacchetti da utilizzare
 library(raster)
 
-setwd("C:/lab/Greenland") # Windows
+setwd("C:/lab/Greenland") # setto la cartella di lavoro su Windows
 
-# Importo dati come al solito
+# Importo l'immagine
 lst2000 <- raster("lst_2000.tif")
 lst2000
 
-# Exercise: import all the data
+# Esercizio: importa tutte le immagini
 lst2005 <- raster("lst_2005.tif")
 lst2010 <- raster("lst_2010.tif")
 lst2015 <- raster("lst_2015.tif")
 
-# Exercise: multiframe of greenland data
+# Esercizio: crea un multiframe con tutte le immagini cambiando la scala di colori
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
 
 par(mfrow=c(2,2))
@@ -22,16 +23,16 @@ plot(lst2005, col=cl)
 plot(lst2010, col=cl)
 plot(lst2015, col=cl)
 
-# Import the whole set altogether!
+# Importa tutto le immagini contemporaneamente
 rlist <- list.files(pattern="lst") # creo la lista
-rlist
+rlist # Visualizzo i parametri
 import <- lapply(rlist, raster) # importo tutto assieme
-import
+import # Visualizzo i parametri
 
 tgr <- stack(import) # crea un unico file con i 4 layer
-tgr
+tgr # Visualizzo i parametri
 
-plot(tgr, col=cl) # plot di tutti e 4
+plot(tgr, col=cl) # plot di tutte e 4 le immagini
 plot(tgr[[1]], col=cl) # plot del 1° elemento
 
 plotRGB(tgr, r=1, g=2, b=3, stretch="lin") # sovrapposizione temporale
@@ -40,21 +41,24 @@ plotRGB(tgr, r=1, g=2, b=3, stretch="lin") # sovrapposizione temporale
 ### Example 2: NO2 decrease during the lockdown period
 ######################################################
 
+# Carico e installo i pacchetti da utilizzare
 library(raster)
-setwd("C:/lab/EN") # Windows
+setwd("C:/lab/EN") # setto la cartella di lavoro su Windows
 
 # Importo il primo layer
 en01 <- raster("EN_0001.png")
 
-cl <- colorRampPalette(c('red','orange','yellow'))(100)
+cl <- colorRampPalette(c('red','orange','yellow'))(100) # cambio la scala di colori
 plot(en01, col=cl)
 
-en13 <- raster("EN_0013.png")
+
+# Importo il layer
+en13 <- raster("EN_0013.png") 
 plot(en13, col=cl)
 
-# Let's import the whole set (altogether!)
+# Importo tutti i layer assieme
 
-# Exercise: import the whole as in the Greenland example by the following steps:
+# Esercizio: importo tutto assieme come nell'esempio Greenland con i seguenti passaggi:
 # list.files, lapply, stack
 
 rlist <- list.files(pattern="EN") # creo la lista
@@ -63,48 +67,23 @@ en <- stack(rimp) # crea un unico file con i 13 layer
 
 plot(en, col=cl) # plotto tutto assieme
 
-# Exercise: plot EN01 besides EN13
+# Esercizio: plotta EN01 affianco a EN13
 par(mfrow=c(1,2)) # modo 1 con multiframe
 plot(en[[1]], col=cl)
 plot(en[[13]], col=cl)
 
-# or:
+# o:
 en113 <- stack(en[[1]], en[[13]]) # modo 2 con stack
 plot(en113, col=cl)
 
-# let's make the difference:
+# facciamo la differenza:
 difen <- en[[1]] - en[[13]]
 cldif <- colorRampPalette(c('blue','white','red'))(100)
 plot(difen, col=cldif)
 
-# plotRGB of three files together
+# plotRGB dei tre file assieme
 plotRGB(en, r=1, g=7, b=13, stretch="lin")
 plotRGB(en, r=1, g=7, b=13, stretch="hist")
 
 sessionInfo() #dà info sula sessione di R
 Sys.time() #dà info sulla data e ora attuale
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
