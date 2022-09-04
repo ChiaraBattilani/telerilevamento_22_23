cfd40da14de1b3ff2ef672c02e02140e769b58ad
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-09-04 12:47:23 +0200

    Update 04_R_code_classification.r

--- 04_R_code_classification.r
+++ 04_R_code_classification.r
@@ -1,57 +1,58 @@
-library(raster)
+library(raster) # Carico i pacchetti
 library(RStoolbox)
 
-setwd("C:/lab")
+setwd("C:/lab") # setto la cartella di lavoro su Windows
 
-#data import
+# Importo l'immagine RGB tramite "brick"
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 
-plotRGB(so, 1, 2, 3, stretch="lin")
+plotRGB(so, 1, 2, 3, stretch="lin") # plot in forma lineare e a istogramma
 plotRGB(so, 1, 2, 3, stretch="hist")
 
-# Classifying the solar data
+# Classifico l'immagine in base alla disposizione dei pixel nello spazio delle tre bande, e per farlo
+# utilizzo "unsuperClass", con cui scelgo il nmero di classi volute
 soc <- unsuperClass(so, nClasses=3)
 
-cl <- colorRampPalette(c('yellow','black','red'))(100)
+cl <- colorRampPalette(c('yellow','black','red'))(100) # creo una nuova scala di colori
 plot(soc$map, col=cl)
 
-# set.seed can be used for repeating the experiment in the same manner for N times
+# set.seed può essere utilizzato per ripetere l'esperimento nella stessa maniera per N volte
 # http://rfunction.com/archives/62
 
 ###################
+# Grand Canyon
 
-# Day 2 Grand Canyon
-
-gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg") # Importo l'immagine e visualizzo i parametri
 gc
 
+# Bande:
 # rosso = 1
 # verde = 2
 # blu = 3
 
 plotRGB(gc, r=1, g=2, b=3, stretch="lin")
 
-# cange the stretch to histogram stretching (ho così un maggior contrasto)
+# cambio lo stretch modalità istogramma, così ho un maggior contrasto
 plotRGB(gc, r=1, g=2, b=3, stretch="hist")
 
-# classification
+# classificazione
 gcclass2 <- unsuperClass(gc, nClasses=2)
 gcclass2
 
 plot(gcclass2$map)
 # set.seed(17) fa solo una delle N ripetizioni fatte, così posso mantenere la stessa classificazione
 
-# Exercise: classify the map with 4 classes
+# Esercizio: classifica la mappa in 4 classi
 gcclass4 <- unsuperClass(gc, nClasses=4)
 gcclass4
 
-clc <- colorRampPalette(c('yellow','red','blue', 'black'))(100)
+clc <- colorRampPalette(c('yellow','red','blue', 'black'))(100) # creo una scala di colori
 plot(gcclass4$map, col=clc)
 
-# Compare the classified map with te original set
+# Confronto la mappa con la classificazione con quella originale
 par(mfrow=c(2,1))
 plot(gcclass4$map, col=clc)
 plotRGB(gc, r=1, g=2, b=3, stretch="hist")
 
-st <- stack(gc, gcclass4$map)
+st <- stack(gc, gcclass4$map) # visualizzo anche tramite uno stack
 plot(st)
