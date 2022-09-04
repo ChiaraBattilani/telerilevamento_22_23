26a281df3b3d042c3570408e55c92a160c632ed6
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-09-04 13:26:12 +0200

    Update 05_R_code_land_cover.r

--- 05_R_code_land_cover.r
+++ 05_R_code_land_cover.r
@@ -1,17 +1,17 @@
-# Code for generating land cover from satellite image
-
+# Codice per generare la land cover da un'immagine satellitare
 # install.packages("ggplot2") # pacchetto che visualizza dati in modo elegante
-# for info: https://ggplot2-book.org/
+# per info: https://ggplot2-book.org/
 # install.packages("patchwork") # pacchetto per semplici multiframe
-# for info: https://patchwork.data-imaginist.com/
+# per info: https://patchwork.data-imaginist.com/
 
-library(raster)
-library(RStoolbox) # for classification
+library(raster) # Installo i pacchetti
+library(RStoolbox) # per classificazione
 library(ggplot2)
 library(patchwork)
 
-setwd("C:/lab")
+setwd("C:/lab") # Setto la cartella di lavoro su Windows
 
+# Bande:
 # NIR = 1
 # r = 2
 # g = 3
@@ -20,63 +20,65 @@ setwd("C:/lab")
 l92 <- brick("defor1_.jpg") # img del 1992
 plotRGB(l92, 1, 2, 3, stretch="lin") # non scrivo r=1, g=2, B=3 perchè so che rgb sono in fila come 1 2 3
 
-# Exercise: import defor2 and plot both in a single window
+# Esercizio: importo defor2 e plotto entrambe le immagini in una finestra unica
 l06 <- brick("defor2_.jpg") # img del 2006
 
 par(mfrow=c(2,1))
 plotRGB(l92, 1, 2, 3, stretch="lin")
 plotRGB(l06, 1, 2, 3, stretch="lin")
 
-# making a simple multiframe with ggplot2
+# Faccio un semplice multiframe con ggplot2
 ggRGB(l92, 1, 2, 3, stretch="lin")
 ggRGB(l06, 1, 2, 3, stretch="lin")
 
 p1 <- ggRGB(l92, 1, 2, 3, stretch="lin") # assegno i plot a un nome così ottengo due oggetti
 p2 <- ggRGB(l06, 1, 2, 3, stretch="lin")
-p1+p2 # thanks to patchwork! uno affianco all'altro
+p1+p2 # grazie a patchwork posso affiancare le immagini
 p1/p2 # uno sotto all'altro
 
-# classification
+# Classificazione
 l92c <- unsuperClass(l92, nClasses=2) # ottego un modello di l92
-l92c
+l92c # visualizzo i parametri
 plot(l92c$map)
-# class 1 : agricultural areas (+ water)
-# class 2 : forest
+# classe 1 : aree agricole (+ acqua)
+# classe 2 : foresta
 
-# Exercise: classify the Landsat image from 2006
+# Esercizio: classifica l'immagine Landsat del 2006 tramite "unsuperClass"
 l06c <- unsuperClass(l06, nClasses=2)
 l06c
 plot(l06c$map)
-# class 1 : forest
-# class 2 : agricultural areas (+ water)
+# classe 1 : foresta
+# classe 2 : aree agricole (+ acqua)
 
-# Frequencies
+# Frequenze: calcolo la frequenza di pixel apparteneneti alla classe della foresta e alla classe delle aree agricole (+ acqua)
 freq(l92c$map)
-# class 1:  35333 pixels (agricultural areas (+ water))
-# class 2: 305959 pixels (forest)
+# class 1:  35333 pixels (aree agricole (+ acqua))
+# class 2: 305959 pixels (foresta)
 
+# Pixel totali
 tot92 <- 341292
 
-# proportion of classes
+# Proporzione della classe foresta
 prop_forest_92 <- 305959 / tot92
 
-# percent of classes
+# Percentuale della classe foresta
 perc_forest_92 <- 305959 * 100 / tot92
 
-# Exercise: calculate the percentage of agricultural areas in 1992
-# method 1
+# Eserczio: calcola la percentuale delle aree agricole del 1992
+# metodo 1
 perc_agr_92 <- 100 - perc_forest_92
-# method 2
+# metodo 2
 perc_agr_92 <- 35333 * 100 / tot92
 
 # percent_forest_92: 89.64728
 # percent_agr_92: 10.35272
 
+# Frequenza 2006
 freq(l06c$map)
 # class 1: 178151 pixels (forest)
 # class 2: 164575 pixels (agricultural areas (+ water))
 
-# percentage 2006
+# Percentuale 2006
 tot06 <- 342726
 percent_forest_06 <- 178151 * 100 / tot06
 percent_agr_06 <- 100 - percent_forest_06
@@ -84,25 +86,26 @@ percent_agr_06 <- 100 - percent_forest_06
 # percent_forest_06: 51.98059
 # percent_agr_06: 48.01941
 
-# FINAL DATA:
+# DATI FINALI:
 # percent_forest_92: 89.64728
 # percent_agr_92: 10.35272
 # percent_forest_06: 51.98059
 # percent_agr_06: 48.01941
 
-# Let's build a dataframe with our data
-# Columns (fields)
+# Costruire un dataframe con i dati ottenuti
+# Colonne (campi)
 class <- c("Forest", "Agriculture")
 percent_1992 <- c(89.64728, 10.35272)
 percent_2006 <- c(51.98059, 48.01941)
 
+# la prima colonna è la classe, la seconda le percentuali del 1992 e la terza le percentuali del 2006
 multitemporal <- data.frame(class, percent_1992, percent_2006)
 
 # 1992
 ggplot(multitemporal, aes(x=class, y=percent_1992, color=class)) + 
 geom_bar(stat="identity", fill="white")
 
-# Exercise: make the same graph for 2006
+# Esercizio: fai ostesso grafico per il 2006
 ggplot(multitemporal, aes(x=class, y=percent_2006, color=class)) + 
 geom_bar(stat="identity", fill="white")
 
@@ -116,16 +119,3 @@ pdf("percentages_2006.pdf")
 ggplot(multitemporal, aes(x=class, y=percent_2006, color=class)) + 
 geom_bar(stat="identity", fill="white")
 dev.off()
-
-
-
-
-
-
-
-
-
-
-
-
-
