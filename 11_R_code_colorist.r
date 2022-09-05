89500ce6cdfc49a5ba3b4664ae2f1d963a088192
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-09-05 15:26:32 +0200

    Update 11_R_code_colorist.r

--- 11_R_code_colorist.r
+++ 11_R_code_colorist.r
@@ -1,23 +1,23 @@
 # Packages----
-install.packages("colorist")
+#install.packages("colorist")
  
 library(colorist) # colorist lavora con pile di raster, non con altre tipologie di file
 library(ggplot2)
 
 # Field sparrow----
 
-data("fiespa_occ") # file con dati degli uccelli
+data("fiespa_occ") # leggo file di sistema con dati degli uccelli traite "data"
 
-# creo metriche
+# creo metriche tramite "metrics_pull"
 met1 <- metrics_pull(fiespa_occ)
 
-# creazione palette
+# creo palette tramite "palette_timecycle"
 pal <- palette_timecycle(fiespa_occ)
 
-# creo mappa per capire dove sono i dati, creo mappa multipla
+# creo mappa multipla per capire dove sono i dati
 map_multiples(met1, pal, ncol = 3, labels = names(fiespa_occ))
 
-map_single(met1, pal, layer = 6) #estraggo un solo mese, layer indica il mese
+map_single(met1, pal, layer = 6) #estraggo un solo mese, "layer" indica il mese
 
 # se vogliamo cambiare il colore dentro la mappa
 pl_custom <- palette_timecycle(12, start_hue = 60)
@@ -37,25 +37,14 @@ legend_timecycle(pal, origin_label = "1 jan")
 # mappare il comportamento individuale di un individuo nel tempo, non è più un tempo circolare, è lineare
 
 # fisher ----
+# Carico un nuovo dato
 data("fisher_ud")
-m2 <- metrics_pull(fisher_ud)
-pal2 <- palette_timeline(fisher_ud)
+m2 <- metrics_pull(fisher_ud) # metrica
+pal2 <- palette_timeline(fisher_ud) # palette
 head(pal2)
 
-# map multiple
+# mappa multipla
 map_multiples(m2, pal2, ncol = 3, lambda_i = -12) # lambda lavora sull'opacità
-m2_distill <- metrics_distill(fisher_ud)
-map_single(m2_distill, pal2, lambda_i = -10)
-legend_timeline(pal2)
-
-
-
-
-
-
-
-
-
-
-
-
+m2_distill <- metrics_distill(fisher_ud) # distillo la mappa
+map_single(m2_distill, pal2, lambda_i = -10) # ne estraggo una
+legend_timeline(pal2) # legenda
