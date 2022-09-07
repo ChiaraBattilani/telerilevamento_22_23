af6dd9227ea87f8fa64f0fd3bef57e5869b0c84f
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-09-07 17:41:15 +0200

    Update R_code_esame.r

--- R_code_esame.r
+++ R_code_esame.r
@@ -16,6 +16,15 @@ library (gridExtra)
 # Settiamo la cartella di lavoro, in questo caso su Windows
 setwd ("C:/lab/esame")
 
-# Importiamo le immagini che andremo ad analizzare: 
+# Importiamo le immagini che andremo ad analizzare: tramite "brick" importo l'immagine e la associo a un nome
 mead_2000 <- brick("lakemead_etm_2000188_lrg.jpg")
 mead_2022 <- brick("lakemead_oli2_2022184_lrg.jpg")
+
+# Richiamo i due oggetti e osservo le proprietà
+mead_2000
+mead_2022
+
+# Faccio un primo plot per osservare le tre bande
+plot(mead_2000)
+plot(mead_2022)
+
