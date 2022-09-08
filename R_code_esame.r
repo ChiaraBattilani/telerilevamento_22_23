f9edd25ab103277206993b1d8f4d7854950ba0df
ChiaraBattilani <100957038+ChiaraBattilani@users.noreply.github.com>
2022-09-08 15:33:26 +0200

    Update R_code_esame.r

--- R_code_esame.r
+++ R_code_esame.r
@@ -6,11 +6,11 @@
 library (raster)
 # install.packages ("rasterVis")
 library (rasterVis)
-# install.packages ("RStoolbox") # to calcolate PCA
+# install.packages ("RStoolbox") # per calcolare la PCA
 library (RStoolbox)
-# install.packages ("ggplot2") # to plot with ggplot
+# install.packages ("ggplot2") # per fare i plot con ggplot
 library (ggplot2)
-# install.packages ("gridExtra") # to plot the ggplots together
+# install.packages ("gridExtra") # per fare un plot con più ggplots assieme
 library (gridExtra)
 
 # Settiamo la cartella di lavoro, in questo caso su Windows
@@ -22,15 +22,51 @@ mead_2022 <- brick("lakemead_oli2_2022184_lrg.jpg")
 
 # Richiamo i due oggetti e osservo le proprietà
 mead_2000
+# class      : RasterBrick 
+# dimensions : 4588, 5505, 25256940, 3  (nrow, ncol, ncell, nlayers)
+# resolution : 1, 1  (x, y)
+# extent     : 0, 5505, 0, 4588  (xmin, xmax, ymin, ymax)
+# crs        : NA 
+# source     : lakemead_etm_2000188_lrg.jpg 
+# names      : lakemead_etm_2000188_lrg.1, lakemead_etm_2000188_lrg.2, lakemead_etm_2000188_lrg.3 
+# min values :                          0,                          0,                          0 
+# max values :                        255,                        255,                        255 
+
 mead_2022
+# class      : RasterBrick 
+# dimensions : 4588, 5505, 25256940, 3  (nrow, ncol, ncell, nlayers)
+# resolution : 1, 1  (x, y)
+# extent     : 0, 5505, 0, 4588  (xmin, xmax, ymin, ymax)
+# crs        : NA 
+# source     : lakemead_oli2_2022184_lrg.jpg 
+# names      : lakemead_oli2_2022184_lrg.1, lakemead_oli2_2022184_lrg.2, lakemead_oli2_2022184_lrg.3 
+# min values :                           0,                           0,                           0 
+# max values :                         255,                         255,                         255 
 
 # Faccio un primo plot per osservare le tre bande
 plot(mead_2000)
 plot(mead_2022)
 
-# Cambio la scala di colori per poter fare migliori osservazioni
-cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
-
+# Salvo entrambi i plot in formato png
 png("mead2000_bande.png") 
-plot(mead_2000)
+plot(mead_2000, main="Lago Mead 2000")
 dev.off()
+
+png("mead2022_bande.png") 
+plot(mead_2022, main="Lago Mead 2022")
+dev.off()
+
+# Cambio la scala di colori per poter fare migliori osservazioni
+cl <- colorRampPalette(c("#FFFFCC", "#CEE2AC", "#9BC592", "#65A87D", "#218B6D", "#006E61")) (100)
+
+# Salvo i plot con la nuova scala di colori
+png("mead2000_bande_CL.png") 
+plot(mead_2000, col=cl, main="Lago Mead 2000")
+dev.off()
+
+png("mead2022_bande_CL.png") 
+plot(mead_2022, col=cl, main="Lago Mead 2022")
+dev.off()
+
+# Visualizziamo ora le bande
+
